73821113a6efe80fa7e243b65aa8717dd63eea4c
Simon Vrachliotis <simon@simonswiss.com>
2025-08-26 21:14:11 +1000

    open contact form in new tab

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -38,6 +38,7 @@ export default async function ServicesPage() {
             <div className="mt-10 flex items-center gap-x-6">
               {contactData?.contactForm && (
                 <Link
+                  target="_blank"
                   href={contactData.contactForm}
                   className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
